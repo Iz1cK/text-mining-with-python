061acf5bd645b46bc127009647ee111185cf9355
George Joubran <durd2001@gmail.com>
2022-11-13 09:31:03 +0200

    found the most used word with 4 or more letters

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,12 +1,24 @@
-text = open('./text.txt',encoding="utf8")
-i=0
-nonletters = set()
-for line in text.readlines():
+text = open('HW1/text.txt',encoding="utf8")
+string = text.read().lower()
+words = {}
+
+nonletters = set('\n')
+for line in string:
     wordsinline = line.split(' ')
     for word in wordsinline:
         for char in word.lower():
-            if(char <= 'a' or char >= 'z'):
-                if not char[0] == '\n':    
+            if(char < 'a' or char > 'z'):
+                if not char == '\n':
                     nonletters.add(char)
-print(nonletters)
+
+for char in nonletters:
+    string = string.replace(char,"")
+
+
+string = string.split(' ')
+for word in string:
+    words[str(word)] = words.get(str(word),0) + 1
+four_letter_words = {word:count for word,count in words.items() if len(word) >=4}
+max_word = [word for word,count in four_letter_words.items() if count == max(four_letter_words.values())]
+print(max_word[0] + ": " + str(words.get(max_word[0])))
     
