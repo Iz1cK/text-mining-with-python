7e094fd4261ba951393eee403d19c51c1f39b698
George Joubran <durd2001@gmail.com>
2022-11-13 16:00:01 +0200

    prrobably last version of the homework

--- HW1/HW1.py
+++ HW1/HW1.py
@@ -1,32 +1,30 @@
-#%%
 from matplotlib import pyplot
 
 text = open('text.txt',encoding="utf8")
 string = text.read().lower()
 
 nonletters = set('\n')
-for line in string:
-    wordsinline = line.split(' ')
-    for word in wordsinline:
-        for char in word.lower():
-            if(char < 'a' or char > 'z'):
-                    nonletters.add(char)
+for word in string.split():
+    for char in word.lower():
+        if(char < 'a' or char > 'z'):
+                nonletters.add(char)
 
 for char in nonletters:
     string = string.replace(char,"")
 
 
 words = {}
-string = string.split(' ')
+string = string.split()
 for word in string:
-    words[str(word)] = words.get(str(word),0) + 1
+    words[word] = words.get(word,0) + 1
+    
 four_letter_words = {word:count for word,count in words.items() if len(word) >=4}
 max_word = [word for word,count in four_letter_words.items() if count == max(four_letter_words.values())]
 print(max_word[0] + ": " + str(words.get(max_word[0])))
 
 bigrams = {}
 for i in range(len(string)-1):
-    bigrams[str(string[i] + " " + string[i+1])] = bigrams.get(str(string[i] + " " + string[i+1]),0) + 1
+    bigrams[string[i] + " " + string[i+1]] = bigrams.get(string[i] + " " + string[i+1],0) + 1
 max_bigram = [word for word,count in bigrams.items() if count == max(bigrams.values())]
 print(max_bigram[0] + ": " + str(bigrams.get(max_bigram[0])))
 
@@ -48,6 +46,3 @@ x = range(1,101)
 pyplot.scatter(x,count_100_words)
 for i in range(0,5):
     pyplot.text(x[i],count_100_words[i],top_100_words[i])
-
-
-# %%
